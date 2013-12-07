a6a3bcd4ce4dc8a4f5d3fb71136a1286f6d14b16
Andrey Tikhomirov <admin@vampik.ru>
2013-12-07 19:53:18 +0400

    Invite: proper event register

--- Modules/Invite.lua
+++ Modules/Invite.lua
@@ -2,7 +2,6 @@
 
 function MinutiaeInvite:OnEnable()
 	self:RegisterEvent("PARTY_INVITE_REQUEST")
-	self:RegisterEvent("PARTY_MEMBERS_CHANGED")
 	if not Minutiae.db.global.invite then
 		self:Disable()
 	end
@@ -10,17 +9,18 @@ end
 
 function MinutiaeInvite:OnDisable()
 	self:UnregisterEvent("PARTY_INVITE_REQUEST")
-	self:UnregisterEvent("PARTY_MEMBERS_CHANGED")
 end
 
 function MinutiaeInvite:PARTY_MEMBERS_CHANGED()
 	StaticPopup_Hide("PARTY_INVITE")
+	self:UnregisterEvent("PARTY_MEMBERS_CHANGED")
 end
 
 function MinutiaeInvite:PARTY_INVITE_REQUEST(_, arg)
 	for i=1, GetNumFriends() do
 		local name = GetFriendInfo(i)
 		if name == arg then
+			self:RegisterEvent("PARTY_MEMBERS_CHANGED")
 			AcceptGroup()
 			return
 		end
@@ -28,6 +28,7 @@ function MinutiaeInvite:PARTY_INVITE_REQUEST(_, arg)
 	for i=1, BNGetNumFriends() do
 		local _, _, _, name = BNGetFriendInfo(i)
 		if name == arg then
+			self:RegisterEvent("PARTY_MEMBERS_CHANGED")
 			AcceptGroup()
 			return
 		end
@@ -35,6 +36,7 @@ function MinutiaeInvite:PARTY_INVITE_REQUEST(_, arg)
 	for i=1, GetNumGuildMembers() do
 		local name = GetGuildRosterInfo(i)
 		if name == arg then
+			self:RegisterEvent("PARTY_MEMBERS_CHANGED")
 			AcceptGroup()
 			return
 		end
