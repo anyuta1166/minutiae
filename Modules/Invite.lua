063428cc004283e3aebf5382e7aaffab9cb92903
Andrey Tikhomirov <admin@vampik.ru>
2013-12-07 20:02:18 +0400

    Code reorganization

--- Modules/Invite.lua
+++ Modules/Invite.lua
@@ -16,28 +16,30 @@ function MinutiaeInvite:PARTY_MEMBERS_CHANGED()
 	self:UnregisterEvent("PARTY_MEMBERS_CHANGED")
 end
 
+function MinutiaeInvite:ProcessInvite()
+	self:RegisterEvent("PARTY_MEMBERS_CHANGED")	
+	AcceptGroup()
+end
+
 function MinutiaeInvite:PARTY_INVITE_REQUEST(_, arg)
 	for i=1, GetNumFriends() do
 		local name = GetFriendInfo(i)
 		if name == arg then
-			self:RegisterEvent("PARTY_MEMBERS_CHANGED")
-			AcceptGroup()
+			self:ProcessInvite()
 			return
 		end
 	end
 	for i=1, BNGetNumFriends() do
 		local _, _, _, _, name = BNGetFriendInfo(i)
 		if name == arg then
-			self:RegisterEvent("PARTY_MEMBERS_CHANGED")
-			AcceptGroup()
+			self:ProcessInvite()
 			return
 		end
 	end
 	for i=1, GetNumGuildMembers() do
 		local name = GetGuildRosterInfo(i)
 		if name == arg then
-			self:RegisterEvent("PARTY_MEMBERS_CHANGED")
-			AcceptGroup()
+			self:ProcessInvite()
 			return
 		end
 	end
