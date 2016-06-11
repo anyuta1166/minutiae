e320ece944121ed024099101f8620412a7a5083c
Andrey Tikhomirov <admin@vampik.ru>
2016-06-11 16:28:02 +0300

    Improve Resurrect module

--- Modules/Resurrect.lua
+++ Modules/Resurrect.lua
@@ -1,4 +1,4 @@
-﻿MinutiaeResurrect = Minutiae:NewModule("MinutiaeResurrect", "AceConsole-3.0", "AceEvent-3.0")
+﻿MinutiaeResurrect = Minutiae:NewModule("MinutiaeResurrect", "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0")
 
 function MinutiaeResurrect:OnEnable()
 	self:RegisterEvent("RESURRECT_REQUEST")
@@ -12,6 +12,50 @@ function MinutiaeResurrect:OnDisable()
 end
 
 function MinutiaeResurrect:RESURRECT_REQUEST()
-	AcceptResurrect()
-	StaticPopup_Hide("RESURRECT_REQUEST")
+	self:mResurrectRequest()
+end
+
+function MinutiaeResurrect:mResurrectRequest()
+	if UnitIsDeadOrGhost("player") then
+		if self:CheckGroupCombat() then
+			self:ScheduleTimer("mResurrectRequest", 2)
+		else
+			if ResurrectHasTimer() then
+				self:ScheduleTimer("mResurrectRequest", GetCorpseRecoveryDelay() + 1)
+			else
+				AcceptResurrect()
+				StaticPopup_Hide("RESURRECT")
+				StaticPopup_Hide("RESURRECT_NO_SICKNESS")
+				StaticPopup_Hide("RESURRECT_NO_TIMER")
+			end
+		end
+	end
+end
+
+function MinutiaeResurrect:CheckGroupCombat()
+	if UnitAffectingCombat("player") then
+		return true
+	end
+	if GetNumGroupMembers() > 0 then
+		if IsInRaid() then
+			for i = 1, GetNumGroupMembers() do
+				if UnitAffectingCombat("raid"..i) then
+					return true
+				end
+				if UnitAffectingCombat("raidpet"..i) then
+					return true
+				end
+			end
+		else
+			for i = 1, GetNumGroupMembers() do
+				if UnitAffectingCombat("party"..i) then
+					return true
+				end
+				if UnitAffectingCombat("partypet"..i) then
+					return true
+				end
+			end
+		end
+	end
+	return false
 end
